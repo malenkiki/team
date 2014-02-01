75da5fd250487c004e6b03b5399d24fe5851f002
Michel Petit <petit.michel@gmail.com>
2014-02-01 19:08:55 +0100

    Start some basis for lib

---
+++ teamlib/team.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2013 Michel Petit <petit.michel@gmail.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+import httplib2 as http
+import urllib
+import json
+
+try:
+    from urlparse import urlparse
+except ImportError:
+    from urllib.parse import urlparse
+
+
+class Team(object):
+    def __init__(self, user, password, uri):
+        self.user = user
+        self.password = password
+        self.uri = uri
+        self.headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json; charset=UTF-8'
+        }
+
+    def param(opts={}):
+        return urllib.urlencode(opts)
+
+    def request(self, method='GET', path='', body='', opts={}):
+        target = urlparse(self.uri + path)
+
+        h = http.Http()
+
+        # authentication:
+        h.add_credentials(self.user, self.password)
+
+        response, content = h.request(target.geturl(), method, body, self.headers)
+        print type(content)
+
+    def getAllTimeEntries(self, opts={}):
+        self.request(path='/time_entries.json', opts=opts)
+
